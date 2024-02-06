a80d50a24dcf707fdeb29e2b54d8ee5f690ab594
Mccoy K <104038316+AuroraDoc@users.noreply.github.com>
2024-02-06 16:36:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -47,44 +47,32 @@ class Replay:
         orange_team = {}
 # might want to add in the ball stat later if we can get possession and pressure percentages
         for idx in range(0, team_size-1):
-            player = {}
-            player.update({"name": data["blue"]["players"][idx]["name"]}) 
-            # update creates a new dictionary everytime. So initialize it once and predefine all the stats as keys and put the json in the val
-            # Core
-            player.update({"core_stats": data["blue"]["players"][idx]["stats"]["core"]})
-            # Boost
-            player.update({"bpm": data["blue"]["players"][idx]["stats"]["boost"]["bpm"]})
-            player.update({"amount_used_while_supersonic": data["blue"]["players"][idx]["stats"]["boost"]["amount_used_while_supersonic"]})
-            player.update({"count_collected_big": data["blue"]["players"][idx]["stats"]["boost"]["count_collected_big"]})
-            player.update({"count_collected_small": data["blue"]["players"][idx]["stats"]["boost"]["count_collected_small"]})
-            # Movement
-            player.update({"avg_speed": data["blue"]["players"][idx]["stats"]["movement"]["avg_speed"]})
-            # Position
-            player.update({"percent_defensive_half": data["blue"]["players"][idx]["stats"]["positioning"]["percent_defensive_half"]})
-            player.update({"percent_offensive_half": data["blue"]["players"][idx]["stats"]["positioning"]["percent_offensive_half"]})
+            player = {"name": data["blue"]["players"][idx]["name"],
+                     "core_stats": data["blue"]["players"][idx]["stats"]["core"],
+                     "bpm": data["blue"]["players"][idx]["stats"]["boost"]["bpm"],
+                     "amount_used_while_supersonic": data["blue"]["players"][idx]["stats"]["boost"]["amount_used_while_supersonic"],
+                     "count_collected_big": data["blue"]["players"][idx]["stats"]["boost"]["count_collected_big"],
+                     "count_collected_small": data["blue"]["players"][idx]["stats"]["boost"]["count_collected_small"],
+                     "avg_speed": data["blue"]["players"][idx]["stats"]["movement"]["avg_speed"],
+                     "percent_defensive_half": data["blue"]["players"][idx]["stats"]["positioning"]["percent_defensive_half"],
+                     "percent_offensive_half": data["blue"]["players"][idx]["stats"]["positioning"]["percent_offensive_half"]}
             blue_team.update(player)
             player.clear()
 
         for idx in range(0, team_size-1):
-            player = {}
-            player.update({"name": data["orange"]["players"][idx]["name"]})
-            # Core
-            player.update({"core_stats": data["orange"]["players"][idx]["stats"]["core"]})
-            # Boost
-            player.update({"bpm": data["orange"]["players"][idx]["stats"]["boost"]["bpm"]})
-            player.update({"amount_used_while_supersonic": data["orange"]["players"][idx]["stats"]["boost"]["amount_used_while_supersonic"]})
-            player.update({"count_collected_big": data["orange"]["players"][idx]["stats"]["boost"]["count_collected_big"]})
-            player.update({"count_collected_small": data["orange"]["players"][idx]["stats"]["boost"]["count_collected_small"]})
-            # Movement
-            player.update({"avg_speed": data["orange"]["players"][idx]["stats"]["movement"]["avg_speed"]})
-            # Position
-            player.update({"percent_defensive_half": data["orange"]["players"][idx]["stats"]["positioning"]["percent_defensive_half"]})
-            player.update({"percent_offensive_half": data["orange"]["players"][idx]["stats"]["positioning"]["percent_offensive_half"]})
+            player = {"name": data["orange"]["players"][idx]["name"],
+                     "core_stats": data["orange"]["players"][idx]["stats"]["core"],
+                     "bpm": data["orange"]["players"][idx]["stats"]["boost"]["bpm"],
+                     "amount_used_while_supersonic": data["orange"]["players"][idx]["stats"]["boost"]["amount_used_while_supersonic"],
+                     "count_collected_big": data["orange"]["players"][idx]["stats"]["boost"]["count_collected_big"],
+                     "count_collected_small": data["orange"]["players"][idx]["stats"]["boost"]["count_collected_small"],
+                     "avg_speed": data["orange"]["players"][idx]["stats"]["movement"]["avg_speed"],
+                     "percent_defensive_half": data["orange"]["players"][idx]["stats"]["positioning"]["percent_defensive_half"],
+                     "percent_offensive_half": data["orange"]["players"][idx]["stats"]["positioning"]["percent_offensive_half"]}
             orange_team.update(player)
             player.clear()
-        game = {}
-        game.update({"blue": blue_team})
-        game.update({"orange": orange_team})
+        game = {"blue": blue_team,
+               "orange": orange_team} # like this with the players
         return game
 def main():
     file = Replay("Path to file you want to use!")
